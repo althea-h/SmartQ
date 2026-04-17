c2e7dfe4a389d176afd52c63759fa073240c3229
Wenoxxxx <owenoxj@gmail.com>
2026-04-17 15:44:17 +0800

    feat: ui dashboard boilerplater, ready to style

---
+++ client/pages/admin/dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/png" href="../../assets/logo/sq.png">
+
+  <link rel="stylesheet" href="../../assets/css/admin/dashboard.css">
+  <link rel="stylesheet" href="../../assets/css/main.css">
+
+
+  <title>SQ | Dashboard</title>
+</head>
+
+<body>
+  <h1>Hello</h1>
+
+</body>
+
+</html>
